7f10e3ba507a1b3818c0db1ad976b51b3d20018a
John Paul Bartolome <johnpaulbartolome30@gmail.com>
2024-12-05 00:37:08 +0800

    Fix bugs in building the app in windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart'; // Import sqflite_common_ffi
 import 'pages/login_page.dart';
 import 'pages/engineer_login.dart';
 import 'pages/account_settings.dart';
@@ -11,18 +12,18 @@ import 'pages/sublot_config.dart';
 import 'pages/review_item.dart';
 import 'pages/revise_item.dart';
 import 'pages/operator_login.dart';
-import 'pages/scan_item.dart';
 import 'database_helper.dart';
-import 'utils/db_path_printer.dart';
-import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  final dbHelper = DatabaseHelper();
-  await dbHelper.database;
-  await DbPathPrinter.printPath();
-  
+
+  // Initialize sqflite_common_ffi for desktop platforms
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+
+  // Ensure database initialization
+  await DatabaseHelper().database;
+
   runApp(const MyApp());
 }
 
@@ -35,68 +36,32 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'QR Barcode System',
       home: const LoginPage(),
-      onGenerateRoute: (settings) {
-        if (settings.name == '/scan-item') {
-          final args = settings.arguments as Map<String, dynamic>;
-          return MaterialPageRoute(
-            builder: (context) => ScanItem(
-              itemName: args['itemName'],
-              poNo: args['poNo'],
-              lotNumber: args['lotNumber'],
-              content: args['content'],
-              qtyPerBox: args['qtyPerBox'],
-              operatorScanId: args['operatorScanId'],
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/engineer-login': (context) => const EngineerLogin(),
+        '/operator-login': (context) => const OperatorLogin(),
+        '/account-settings': (context) => const AccountSettings(),
+        '/manage-accounts': (context) => const ManageAccounts(),
+        '/new-account': (context) => const NewAccount(),
+        '/edit-user': (context) => EditUser(
+            user: ModalRoute.of(context)!.settings.arguments
+                as Map<String, dynamic>),
+        '/item-masterlist': (context) => const ItemMasterlist(),
+        '/register-item': (context) => const RegisterItem(),
+        '/sublot-config': (context) => SublotConfig(
+              itemName: '',
+              countingCodes: const <Map<String, String>>[],
             ),
-          );
-        }
-        
-        // Handle other routes
-        switch (settings.name) {
-          case '/login':
-            return MaterialPageRoute(builder: (context) => const LoginPage());
-          case '/engineer-login':
-            return MaterialPageRoute(builder: (context) => const EngineerLogin());
-          case '/operator-login':
-            return MaterialPageRoute(builder: (context) => const OperatorLogin());
-          case '/account-settings':
-            return MaterialPageRoute(builder: (context) => const AccountSettings());
-          case '/manage-accounts':
-            return MaterialPageRoute(builder: (context) => const ManageAccounts());
-          case '/new-account':
-            return MaterialPageRoute(builder: (context) => const NewAccount());
-          case '/edit-user':
-            final args = settings.arguments as Map<String, dynamic>;
-            return MaterialPageRoute(
-              builder: (context) => EditUser(user: args),
-            );
-          case '/item-masterlist':
-            return MaterialPageRoute(builder: (context) => const ItemMasterlist());
-          case '/register-item':
-            return MaterialPageRoute(builder: (context) => const RegisterItem());
-          case '/sublot-config':
-            return MaterialPageRoute(
-              builder: (context) => SublotConfig(
-                itemName: '',
-                countingCodes: const <Map<String, String>>[],
-              ),
-            );
-          case '/review-item':
-            return MaterialPageRoute(
-              builder: (context) => ReviewItem(
-                itemName: '',
-                revision: '',
-                codeCount: 0,
-                codes: [],
-              ),
-            );
-          case '/revise-item':
-            final args = settings.arguments as Map<String, dynamic>;
-            return MaterialPageRoute(
-              builder: (context) => ReviseItem(item: args),
-            );
-          default:
-            return null;
-        }
+        '/review-item': (context) => ReviewItem(
+              itemName: '',
+              revision: '',
+              codeCount: 0,
+              codes: [],
+            ),
+        '/revise-item': (context) => ReviseItem(
+              item: ModalRoute.of(context)!.settings.arguments
+                  as Map<String, dynamic>,
+            ),
       },
     );
   }
